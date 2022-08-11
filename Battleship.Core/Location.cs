637b29c0ce13f01cc74a04f429adf078fe0c5938
Weronika Łabaj <weronika.labaj@gmail.com>
2022-08-11 12:04:19 +0200

    - Add Location value object to represent cell location and convert between indexes and literal representation: column represented by letter, row by number - Add tests to cover basic valid values converstions and edge cases

---
+++ Battleship.Core/Location.cs
@@ -0,0 +1,43 @@
+﻿namespace Battleship.Core
+{
+    public readonly struct Location
+    {
+        public int ColumnIndex { get; init; }
+        public int RowIndex { get; init; }
+        public string Cooridnates { get { return $"{(char)(ColumnIndex + 65)}{RowIndex + 1}"; } }
+
+        public Location(int rowIndex, int columnIndex)
+        {
+            if(columnIndex < 0 || columnIndex > 25)
+            {
+                throw new ArgumentOutOfRangeException(nameof(columnIndex), "Valid board location index is a number in the range 0 to 25.");
+            }
+
+            if (rowIndex < 0 || rowIndex > 25)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rowIndex), "Valid board location index is a number in the range 0 to 25.");
+            }
+
+            ColumnIndex = columnIndex;
+            RowIndex = rowIndex;
+        }
+
+        public Location(string literalCoordinates)
+        {
+            int parsedColumnNumber;
+            var columnIsValidNumber = int.TryParse(literalCoordinates.Substring(1), out parsedColumnNumber);
+
+
+            var tmpColIndex = literalCoordinates[0] - 'A';
+            var tmpRowIndex = parsedColumnNumber - 1;
+
+            if (!columnIsValidNumber || tmpColIndex < 0 || tmpColIndex > 25 || tmpRowIndex < 0 || tmpRowIndex > 25)
+            {
+                throw new ArgumentOutOfRangeException(nameof(literalCoordinates), "Valid board coordination has format '[A-Z][1-25].'");
+            }
+
+            RowIndex = tmpRowIndex;
+            ColumnIndex = tmpColIndex;
+        }
+    }
+}
---
+++ Battleship.Tests/LocationTests.cs
@@ -0,0 +1,61 @@
+﻿using Battleship.Core;
+using NUnit.Framework;
+using System;
+
+namespace Battleship.Tests
+{
+    [TestFixture]
+    public class LocationTests
+    {
+
+        [TestCase(0, 0, "A1")]
+        [TestCase(9, 0, "A10")]
+        [TestCase(0, 9, "J1")]
+        [TestCase(1, 1, "B2")]
+        [TestCase(9, 9, "J10")]
+        [TestCase(10, 10, "K11")]
+        [TestCase(25, 25, "Z26")]
+        public void BoardLocationToLiteralCoordinates(int rowIndex, int colIndex, string expectedLiteralCoordinates)
+        {
+            var boardLocation = new Location(rowIndex, colIndex);
+
+            Assert.AreEqual(expectedLiteralCoordinates, boardLocation.Cooridnates);
+        }
+
+
+        [TestCase(0, 0, "A1")]
+        [TestCase(9, 0, "J1")]
+        [TestCase(0, 9, "A10")]
+        [TestCase(1, 1, "B2")]
+        [TestCase(9, 9, "J10")]
+        [TestCase(10, 10, "K11")]
+        [TestCase(25, 25, "Z26")]
+        public void LiteralCoordinatesToBoardLocation(int expRowIndex, int expColIndex, string literalCoordinates)
+        {
+            var boardLocation = new Location(literalCoordinates);
+
+            Assert.AreEqual(expRowIndex, boardLocation.ColumnIndex);
+            Assert.AreEqual(expColIndex, boardLocation.RowIndex);
+        }
+
+        [TestCase("A27")]
+        [TestCase("a1")]
+        [TestCase("11")]
+        [TestCase("A0")]
+        public void EnforcesConstraintsOnBoardLocationIndexes(string literalCoordinates)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() 
+                => new Location(literalCoordinates));
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        [TestCase(26, 0)]
+        [TestCase(0, 26)]
+        public void EnforcesConstraintsOnBoardLocationCoordinates(int rowIndex, int columnIndex)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(()
+                => new Location(rowIndex, columnIndex));
+        }
+    }
+}
